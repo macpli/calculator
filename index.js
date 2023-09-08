6821add789377bc2de564c6b68fd0792a6ed2fea
Maciej Pliszek <pliszekmaciej0@gmail.com>
2023-09-08 23:40:03 +0200

    add: fully functioning add feature

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-console.log('hello world!')
+let num1, num2, currNum, operator;
+currNum = '';
 
-//let numbersContainer = document.querySelector('.numbers');
 let display = document.querySelector('.display');
 display.textContent ='';
+let displayNumber;
+
 let row1 = document.querySelector('.row-1');
 let row2 = document.querySelector('.row-2');
 let row3 = document.querySelector('.row-3');
 
-let num1, num2, currNum, operator;
-currNum = '';
-operator = '+';
-num1 = 16;
-num2 = 4;
+const opAdd = document.querySelector('#add').addEventListener('click', function(){
+    num1 = currNum;
+    operator = '+';
+    displayNumber = num1 + '+';
+    updateDisplay(displayNumber);
+    currNum = '';
+});
+const opSub = document.querySelector('#sub');
+const opMul = document.querySelector('#mul');
+const opDiv = document.querySelector('#div');
+
+const opEqu = document.querySelector('#equ').addEventListener('click', function(){
+    num2 = Number(currNum);
+    if(num1 == undefined){
+        num1 = 0;
+    }
+
+    if(operator == '+'){
+        Add(Number(num1), Number(num2));
+    }
+
+
+    
+    
+});
 
 const Add = function(num1, num2){
-    let result = num1 + num2;
-    console.log(result);
+    currNum = Number(num1) + Number(num2);
+    currNum = updateCurrentNumber(currNum, '');
+    updateDisplay(currNum)
+    return currNum;
 }
 
 const Subtract = function(num1, num2){
@@ -53,6 +77,15 @@ const generateCalcNums = function() {
         btn.setAttribute('id', idNumber);
         btn.addEventListener('click', function() {
             currNum = updateCurrentNumber(currNum, idNumber);
+            if(displayNumber == undefined){ displayNumber = '' };
+
+            if(num1 == undefined){
+                displayNumber = String(currNum);
+                updateDisplay(displayNumber);
+            } else if (num1 != undefined){
+                displayNumber = String(num1) + '+' + String(currNum);
+                updateDisplay(displayNumber);
+            }
         });
         row1.append(btn);
     }
@@ -63,27 +96,66 @@ const generateCalcNums = function() {
         btn.setAttribute('id', 4+i);
         btn.addEventListener('click', function() {
             currNum = updateCurrentNumber(currNum, idNumber);
+            if(displayNumber == undefined){ displayNumber = '' };
+
+            if(num1 == undefined){
+                displayNumber = String(currNum);
+                updateDisplay(displayNumber);
+            } else if (num1 != undefined){
+                displayNumber = String(num1) + '+' + String(currNum);
+                updateDisplay(displayNumber);
+            }
         });
         row2.append(btn);
     }
+
+    // TODO
+    // const onClickNum = function(idNumber, currNum, num1, displayNumber){
+    //     currNum = updateCurrentNumber(currNum, idNumber);
+
+    //         if(displayNumber == undefined){ displayNumber = '' };
+
+    //         if(num1 == undefined){
+    //             displayNumber = String(currNum);
+    //             updateDisplay(displayNumber);
+    //         } else if (num1 != undefined){
+    //             displayNumber = String(num1) + '+' + String(currNum);
+    //             updateDisplay(displayNumber);
+    //         }
+    //     return currNum, displayNumber;
+    // }
+
     for(let i = 0; i < 3; i++){
         let idNumber = 1 + i;
         let btn = document.createElement('button');
         btn.textContent = 1 + i;
         btn.setAttribute('id', 1+i);
         btn.addEventListener('click', function() {
+            // TODO
+            // onClickNum(idNumber, currNum, num1, displayNumber);
             currNum = updateCurrentNumber(currNum, idNumber);
+
+            if(displayNumber == undefined){ displayNumber = '' };
+
+            if(num1 == undefined){
+                displayNumber = String(currNum);
+                updateDisplay(displayNumber);
+            } else if (num1 != undefined){
+                displayNumber = String(num1) + '+' + String(currNum);
+                updateDisplay(displayNumber);
+            }
         });
         row3.append(btn);
     }
 }
 
 const updateCurrentNumber = function(currNum, num){
-    console.log('entry currNum: ' + currNum);
     currNum = String(currNum) + String(num);
-    console.log('new currNum: ' + currNum)
-    display.textContent = currNum;
     return currNum;
+}
+
+const updateDisplay = function(displayNumer){
+    display.textContent = String(displayNumer);
 }
 
 generateCalcNums();
